ad7136135e8c1a9671850b9b6af98ee61377b5b6
Kevin Perez <kevindperezm@gmail.com>
2015-06-15 19:39:14 -0500

    añadir el primer usuario administrador
    Este commit añade el primer usuario que se usará para probar la feature
    de inicio de sesión.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Usuario;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,6 +16,13 @@ class DatabaseSeeder extends Seeder {
 		Model::unguard();
 
 		// $this->call('UserTableSeeder');
+
+		Usuario::create(['email'              => 'kevindperezm@gmail.com',
+			               'contrasena_cifrada' => 'asdfg',
+			               'nombre'             => 'Kevin',
+			               'apellido'           => 'Perez',
+			               'idioma'             => 'es',
+			               'rol_id'             => 1]);
 	}
 
 }
