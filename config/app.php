1d0736034b42338a535609acfa6ddb700cb52cf6
Kevin Perez <kevindperezm@gmail.com>
2015-06-18 19:17:04 -0500

    instalar html helpers
    Laravel 5 ya no incluye los HTML helpers que venían con Laravel 4.2.
    Este commit los instala para que sea sencillo crear formularios, tablas,
    enlaces y demás.

--- config/app.php
+++ config/app.php
@@ -145,6 +145,11 @@ return [
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
 
+		/*
+		 * ServiceProviders de terceros...
+		 */
+		'Illuminate\Html\HtmlServiceProvider',
+
 	],
 
 	/*
@@ -192,6 +197,12 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+
+		/**
+		 * Facades de terceros...
+		 */
+		'Form'      => 'Illuminate\Html\FormFacade',
+		'HTML'      => 'Illuminate\Html\HtmlFacade',
 
 	],
 
