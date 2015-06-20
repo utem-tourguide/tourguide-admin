42aead53483bf9940bd529b7fa151b3f8bab5d32
Kevin Perez <kevindperezm@gmail.com>
2015-06-20 02:04:53 -0500

    añadir Usuario::cifrarContrasena
    Este nuevo método es capaz de cifrar una contraseña en texto plano y
    devolver su versión cifrada.

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -13,4 +13,16 @@ class Usuario extends Model {
                          'rol_id',
                         ];
 
+  /**
+   * Cifra una contraseña en texto plano y devuelve su versión cifrada.
+   *
+   * @param string $contrasena
+   * @return string
+   */
+  public static function cifrarContrasena($contrasena) {
+    $factor_cifrado = 31;
+    $salt = "$2y$$factor_cifrado" . substr(md5(uniqid(rand(), true)), 0, 22);
+    return crypt($contrasena, $salt);
+  }
+
 }
