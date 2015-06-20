1dbc1c1bc4a1ad29eb212accd468240813a4b121
Kevin Perez <kevindperezm@gmail.com>
2015-06-20 03:47:25 -0500

    añadir SesionesControllerTest
    Esto añade una prueba de unidad para SesionesController. Esta prueba
    garantiza que cada método de SesionesController funciona como debería.

---
+++ tests/Http/Controllers/SesionesControllerTest.php
@@ -0,0 +1,54 @@
+<?php namespace tests\Http\Controllers;
+
+use TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class SesionesControllerTest extends TestCase {
+
+  use WithoutMiddleware;
+
+  /**
+   * @test
+   */
+  public function formulario_para_iniciar_sesion() {
+    $this->route('GET', 'sesiones.entrar');
+
+    $this->assertResponseOk();
+  }
+
+  /**
+   * @test
+   */
+  public function iniciar_sesion_correctamente() {
+    $this->route('POST', 'sesiones.entrar', [
+      'email'      => 'kevindperezm@tourguide.com',
+      'contrasena' => 'asdfg',
+    ]);
+
+    $this->assertSessionHas('usuario_id');
+  }
+
+  /**
+   * @test
+   */
+  public function iniciar_sesion_erroneamente() {
+    $this->route('POST', 'sesiones.entrar', [
+      'email'      => 'usuario@invalido.com',
+      'contrasena' => 'invalida',
+    ]);
+
+    $this->assertRedirectedToRoute('sesiones.entrar');
+    $this->assertFalse($this->app['session.store']->has('usuario_id'));
+  }
+
+  /**
+   * @test
+   */
+  public function cerrar_sesion() {
+    $this->route('GET', 'sesiones.salir');
+
+    $this->assertRedirectedToRoute('sesiones.entrar');
+    $this->assertFalse($this->app['session.store']->has('usuario_id'));
+  }
+
+}
