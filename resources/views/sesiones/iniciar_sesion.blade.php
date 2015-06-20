adadb8185bbfe1acedd2bd897b6d461dd60cef20
Kevin Perez <kevindperezm@gmail.com>
2015-06-20 01:49:42 -0500

    añadir view sesiones.iniciar_sesion
    Esto añade una view con el código necesario para mostrar la pantalla de
    inicio de sesión.

---
+++ resources/views/sesiones/iniciar_sesion.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Entrar a TourGuide Admin</title>
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
+</head>
+<body>
+
+  <div class="container-fluid">
+
+    <div class="row">
+
+      <div class="col-xs-12">
+        <img id="app-logo" src="{{ asset('images/tourguide-logo.jpg') }}">
+
+        <div id="login_form" class="well well-lg">
+          {!! Form::open(['route' => 'iniciar-sesion']) !!}
+            {!! Form::email('email', '', ['class' => 'form-control', 'placeholder' => 'Correo electrónico', 'required']) !!}
+            {!! Form::password('contrasena', ['class' => 'form-control', 'placeholder' => 'Contraseña', 'required']) !!}
+            {!! Form::submit('Entrar', ['class' => 'btn btn-large btn-primary btn-block']) !!}
+          {!! Form::close() !!}
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <footer class="text-center">
+    &copy; 2015 TourGuide Team
+  </footer>
+
+</body>
+</html>
