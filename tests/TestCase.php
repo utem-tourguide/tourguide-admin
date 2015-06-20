962900f870da24540509c2fbe12d33960951dc11
Kevin Perez <kevindperezm@gmail.com>
2015-06-20 03:24:37 -0500

    usar BD en memoria para pruebas de phpunit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,18 +2,27 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
-	/**
-	 * Creates the application.
-	 *
-	 * @return \Illuminate\Foundation\Application
-	 */
-	public function createApplication()
-	{
-		$app = require __DIR__.'/../bootstrap/app.php';
+  /**
+   * Creates the application.
+   *
+   * @return \Illuminate\Foundation\Application
+   */
+  public function createApplication()
+  {
+    putenv('DB_DEFAULT=sqlite_testing');
 
-		$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+    $app = require __DIR__.'/../bootstrap/app.php';
 
-		return $app;
-	}
+    $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+    return $app;
+  }
+
+  public function setUp() {
+    parent::setUp();
+
+    Artisan::call('migrate');
+    Artisan::call('db:seed');
+  }
 
 }
