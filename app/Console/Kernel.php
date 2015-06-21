c87a2c84a18a2ba7d06ef5baab57face3997ce0a
Kevin Perez <kevindperezm@gmail.com>
2015-06-21 02:55:09 -0500

    evitar que command Inspire se ejecute cada hora

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,16 +12,4 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected $commands = [];
 
-	/**
-	 * Define the application's command schedule.
-	 *
-	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-	 * @return void
-	 */
-	protected function schedule(Schedule $schedule)
-	{
-		$schedule->command('inspire')
-				 ->hourly();
-	}
-
 }
