465ff04af09d30b34a7b58af1485cc76451647d7
Dannypein <danieldolores117@gmail.com>
2015-07-01 21:21:22 -0500

    retirar formulario
    se retiro el formulario del index

--- resources/views/usuarios/partials/formulario.blade.php
+++ resources/views/usuarios/partials/formulario.blade.php
@@ -12,6 +12,10 @@
 	<input type="text" class="form-control" name="apellido" value="{{ $usuario->apellido }}">
 </div>
 <div class="form-group">
+	<label>Idioma</label>
+	{!! Form::select('idioma', ['es' => 'Español', 'en' => 'Ingles', 'fr' => 'Frances'], $usuario->idioma) !!}
+</div>
+<div class="form-group">
 	<label>Email:</label>
 	<input type="email" class="form-control" name="email" value="{{ $usuario->email }}">
 </div>
@@ -26,9 +30,5 @@
 <div class="form-group">
 	<label>Confirmar Contraseña:</label>
 	<input type="password" class="form-control" name="confirmar_contrasena">
-</div>
-<div class="form-group">
-	<label>Idioma</label>
-	{!! Form::select('idioma', ['es' => 'Español', 'en' => 'Ingles', 'fr' => 'Frances'], $usuario->idioma) !!}
 </div>
 <input type="submit" value="Guardar" class=" btn btn-primary"><a href="{{ route('usuarios.index') }}" class="btn btn-danger" style="margin: 2em;">Cancelar</a>
