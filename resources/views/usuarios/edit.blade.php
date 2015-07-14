228205eaaec0dbd98ce7ea759c9ab45227b23953
Dannypein <danieldolores117@gmail.com>
2015-07-13 20:17:29 -0500

    cambiar el formulario de edit
    se borro el formulario anterior y se agrefo uno nuevo con los campos
    nuevos

--- resources/views/usuarios/edit.blade.php
+++ resources/views/usuarios/edit.blade.php
@@ -1,17 +1,30 @@
-<html>
-<head>
-	<title>Usuarios - TourGuide Admin</title>
-	<link rel="stylesheet" type="text/css" href="/css/styles.css">
-</head>
-<body>
-	<div class="col-md-3">
-	</div>
-	<div class="col-md-6 form-group">
-		{!! Form::open(['route' => ['usuarios.update', $usuario->id], 'method' => 'PATCH']) !!}
-				@include('usuarios.partials.formulario')
-		{!! Form::close() !!}
-	</div>
-	<div class="col-md-3">
-	</div>
-</body>
-</html>
+{!! Form::open(['id'    => 'formularioEditar']) !!}
+  <div class="form-group">
+	<label>Nombre:</label>
+	<input type="text" class="form-control" name="nombre">
+</div>
+<div class="form-group">
+	<label>Apellido:</label>
+	<input type="text" class="form-control" name="apellido">
+</div>
+<div class="form-group">
+	<label>Idioma</label>
+	<select name="idioma">
+		<option value="es">Español</option>
+		<option value="en">Ingles</option>
+		<option value="fr">Frances</option>
+	</select>
+</div>
+<div class="form-group">
+	<label>Email:</label>
+	<input type="email" class="form-control" name="email">
+</div>
+<div class="form-group">
+	<label>Contraseña:</label>
+	<input type="password" class="form-control" name="contrasena">
+</div>
+<div class="form-group">
+	<label>Confirmar Contraseña:</label>
+	<input type="password" class="form-control" name="confirmar_contrasena">
+</div>
+{!! Form::close() !!}
