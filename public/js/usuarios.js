2116116ad77566e92289bff1bd03ba09b740dc68
Dannypein <danieldolores117@gmail.com>
2015-07-13 20:14:59 -0500

    modificar usuarios js
    se modifico construir fila para usuarios, se creo al dialogo para
    editar, se agrego el formulario y se agrego la funcion de actualizar

--- public/js/usuarios.js
+++ public/js/usuarios.js
@@ -19,13 +19,15 @@ function construirTablaUsuarios(usuario) {
 
 function construirFilaParaUsuarios(usuarios, nuevo) {
   return $(
-    '<tr class="usuarios animado' + (nuevo ? ' nuevo' : '') + '">' +
+    '<tr class="usuarios animado' + (nuevo ? ' nuevo' : '') + '" data-usuario-id="' + usuarios.id + '">' +
       '<td>' + usuarios.id + '</td>' +
       '<td>' + usuarios.nombre + '</td>' +
       '<td>' + usuarios.apellido + '</td>' +
       '<td>' + usuarios.idioma + '</td>' +
       '<td>' + usuarios.email + '</td>' +
       '<td>' + usuarios.rol_id + '</td>' +
+      '<td>' + '<button onclick="mostrarDialogoParaEditar('+ usuarios.id +')" class="btn btn-warning">Editar</button>' + 
+      '<button class="btn btn-danger" onclick="eliminar('+ usuarios.id +')"> Eliminar </button>' + '</td>' +
     '</tr>'
   );
 }
@@ -51,4 +53,42 @@ function guardarNuevoUsuario(formulario, guardarUrl) {
       alert('Hubo un error.');
     }
   })
+}
+
+function mostrarDialogoParaEditar (usuarioId) {
+  $.ajax({
+    method: 'GET',
+    url: '/usuarios/' + usuarioId,
+    success: function(usuarios) {
+      formularioEditar.nombre.value = usuarios.nombre;
+      formularioEditar.apellido.value = usuarios.apellido;
+      formularioEditar.idioma.value = usuarios.idioma;
+      formularioEditar.email.value = usuarios.email;
+      formularioEditar.contrasena.value = usuarios.contrasena;
+      formularioEditar.confirmar_contrasena.value = usuarios.confirmar_contrasena;
+      $(formularioEditar).attr('data-usuario-id', usuarioId);
+      $(usuariosEditar).modal();
+    }
+  });
+}
+
+function actualizarUsuarios(formulario) {
+  id = $(formulario).attr('data-usuario-id');
+  $.ajax({
+    method: 'PATCH',
+    url: '/usuarios/' + id,
+    data: $(formulario).serialize(),
+    success: function(usuario) {
+      $(usuariosEditar).modal('hide'); // Cierra el dialogo de edición
+      actualizarFilaParaUsuarios(usuario);
+    } 
+  });
+}
+
+function actualizarFilaParaUsuarios(usuario) {
+  fila = $('tr[data-usuario-id=' + usuario.id + ']');
+  $(fila.children()[1]).text(usuario.nombre);
+  $(fila.children()[2]).text(usuario.epellido);
+  $(fila.children()[3]).text(usuario.idioma);
+  $(fila.children()[4]).text(usuario.email);
 }
