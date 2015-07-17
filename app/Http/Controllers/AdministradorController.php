9c4271808fae2b0868c6887e5d33e2ba45f4f6e4
Kevin Perez <kevindperezm@gmail.com>
2015-07-17 00:41:44 -0500

    añadir acción AdministradorController@informacion()
    Esta acción renderizará la página para administrar la información de las
    ubicaciones turísticas registradas.

--- app/Http/Controllers/AdministradorController.php
+++ app/Http/Controllers/AdministradorController.php
@@ -1,9 +1,19 @@
 <?php namespace TourGuide\Http\Controllers;
 
+use TourGuide\Models\UbicacionTuristica;
+
 class AdministradorController extends Controller {
 
   public function ubicaciones() {
     return view('ubicaciones.index');
+  }
+
+  public function informacion($ubicacion_id) {
+    $datos = [
+      'ubicacion' => UbicacionTuristica::find($ubicacion_id),
+    ];
+
+    return view('informacion.index', $datos);
   }
 
 }
