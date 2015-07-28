226f22a0bfb4046664e14e7b263faeb664699e64
Kevin Perez <kevindperezm@gmail.com>
2015-07-27 20:44:03 -0500

    añadir rutas para información de ubicaciones
    Esto añade las rutas para la información de las ubicaciones turísticas
    como un subrecurso de las ubicaciones.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,6 +32,7 @@ Route::get('/obtener-app', ['as'   => 'obtener_app',
 
 Route::resource('/usuarios', 'UsuariosController');
 Route::resource('/ubicaciones', 'UbicacionesController');
+Route::resource('ubicaciones.informacion', 'InformacionUbicacionesController');
 
 Route::group(['prefix' => '/administrar'], function() {
   Route::get('/ubicaciones', [
