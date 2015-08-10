6f44aa4729ed25deb261e3b5e69974a398a12093
Kevin Perez <kevindperezm@gmail.com>
2015-08-09 19:34:08 -0500

    añadir archivo image_previewer.js
    Este archivo contiene la definición de un objeto que puede usarse para
    mostrar vistas previas de imágenes sin necesidad de subirlas al
    servidor.
    
    Este código requiere IE10 o superior, o cualquier versión de cualquier
    otro navegador, excepto Opera Mini.

---
+++ public/js/image_previewer.js
@@ -0,0 +1,25 @@
+/**
+ * ImagePreviewer
+ *
+ * Este objeto es capaz de mostrar el archivo de imagen seleccionado en un input de tipo "file".
+ * Es muy útil para mostrar imágenes antes de subirlas al servidor, por ejemplo.
+ */
+function ImagePreviewer(fileInput) {
+  this.fileInput = fileInput;
+}
+
+ImagePreviewer.prototype.render = function(whereToRenderSelector) {
+  this.readUrl(whereToRenderSelector);
+};
+
+ImagePreviewer.prototype.readUrl = function(whereToRenderSelector) {
+  if ( ! this.fileInput.files || ! this.fileInput.files[0]) return false;
+
+  var reader = new FileReader();
+
+  reader.onload = function(e) {
+    $(whereToRenderSelector).attr('src', e.target.result);
+  };
+
+  reader.readAsDataURL(this.fileInput.files[0]);
+};
