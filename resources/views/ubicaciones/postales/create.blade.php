0a2bb676f77e1005126bcd97b87e9dc7f6f6d720
Kevin Perez <kevindperezm@gmail.com>
2015-08-09 21:16:24 -0500

    mostrar correctamente form para editar postal
    Algunos campos del formulario para editar postal no mostraban el valor
    actual de la propiedad de la postal seleccionada. Esto corrige el
    problema.

--- resources/views/ubicaciones/postales/create.blade.php
+++ resources/views/ubicaciones/postales/create.blade.php
@@ -1,16 +1,14 @@
-{!! Form::open(['id' => 'formulario', 'files' => true]) !!}
+{!! Form::model(isset($postal) ? $postal : null, ['id' => 'formulario', 'files' => true]) !!}
   <div class="form-group">
     {!! Form::label('imagen') !!}
     <div class="thumbnail well">
-      <img id="postalImagen" {{ isset($postal) ? "src='{$postal->obtenerImagenUrl()}'" : '' }}>
+      <img id="postalImagen" {{ isset($postal) ? "src={$postal->obtenerImagenUrl()}" : '' }}>
       {!! Form::file('imagen', ['id' => 'postalArchivo', 'class' => 'form-control']) !!}
     </div>
   </div>
 
   <div class="form-group">
     {!! Form::label('precio') !!}
-    {!! Form::text('precio', '', ['class' => 'form-control']) !!}
+    {!! Form::text('precio', null, ['class' => 'form-control']) !!}
   </div>
 {!! Form::close() !!}
-
-
