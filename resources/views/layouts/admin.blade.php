f6c6f1e749c3b270ce3637b51da19a06bbd8e60d
Kevin Perez <kevindperezm@gmail.com>
2015-08-17 15:12:49 -0500

    añadir sidebar a views de administrador

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.main')
 
+@section('body')
+  <div class="row">
+    <aside class="col-md-2">
+      <img id="logo" class="img-responsive" src="{{ asset('images/tourguide-logo.jpg') }}">
+      <h5>TourGuide</h5>
+
+      <nav>
+        <ul class="list-unstyled">
+          <li>{!! link_to_route('dashboard', 'Inicio') !!}</li>
+          <li>{!! link_to_route('administrar.compras', 'Compras') !!}</li>
+          <li>{!! link_to_route('administrar.usuarios', 'Usuarios') !!}</li>
+          <li>{!! link_to_route('administrar.ubicaciones', 'Ubicaciones turísticas') !!}</li>
+        </ul>
+      </nav>
+    </aside>
+    <main class="col-md-10">
+      @yield('content')
+    </main>
+  </div>
+@endsection
+
 @section('scripts')
   @parent
   <script src="/js/crud.js"></script>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,13 +2,16 @@
 <html>
 <head>
   <title>@yield('title') | TourGuide</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @section('styles')
     <link rel="stylesheet" type="text/css" href="/css/styles.css">
   @show
 </head>
 <body>
 <div class="container-fluid">
-  @yield('content')
+  @section('body')
+    @yield('content')
+  @show
 </div>
 
 @section('scripts')
