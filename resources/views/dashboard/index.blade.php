04cda5ce93d15aa120d879df34aa10fd19a011c0
Dannypein <danieldolores117@gmail.com>
2015-08-19 12:23:20 -0500

    crear la bienvenida y las graficas
    se creo el mensaje de bienvenida y las secciones de bienvenida

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,5 +3,29 @@
 @section('title', 'Dashboard')
 
 @section('content')
-  {!! link_to_route('sesiones.destroy', 'Cerrar sesión') !!}
+	<p>Bienvenido:  {{ Auth::user()->nombreCompleto() }}</p>
+	<br>
+	<div>
+	  <div class="col-md-4">
+	  	<p>Ubiciones turísticas más visitadas</p>
+	  </div>
+	  <div class="col-md-4">
+	  	<p>Postales más vendidas</p>
+	  	{{}}
+	  </div>
+	  <div class="col-md-4">
+	  	<p>Idiomas más solicitados</p>
+	  </div>
+	</div>
+	<div>
+		<div>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</div>
+	</div>
 @endsection
+ 
