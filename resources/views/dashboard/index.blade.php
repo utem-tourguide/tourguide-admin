81f21083ec9a7ecf0688150ef6a8cf60545e0f81
Dannypein <danieldolores117@gmail.com>
2015-08-19 13:46:02 -0500

    crear el dashboard
    agregar las tablas y las graficas del dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,28 +3,85 @@
 @section('title', 'Dashboard')
 
 @section('content')
-	<p>Bienvenido:  {{ Auth::user()->nombreCompleto() }}</p>
+	<div class="col-md-6">
+		<strong>Bienvenido:  {{ Auth::user()->nombreCompleto() }}</strong>
+	</div>
+	<div class="col-md-6">
+		<strong>Monto acumulado de la venta de postales: {{$total_Ingresadas[0]->total_SolicitudesIngresadas}}</strong>
+	</div>
+	<br>
 	<br>
 	<div>
 	  <div class="col-md-4">
 	  	<p>Ubiciones turísticas más visitadas</p>
 	  </div>
+
 	  <div class="col-md-4">
 	  	<p>Postales más vendidas</p>
-	  	{{}}
 	  </div>
-	  <div class="col-md-4">
+
+	  <div>
 	  	<p>Idiomas más solicitados</p>
 	  </div>
+
 	</div>
+	<br>
+	<hr style="">
+	<br>
 	<div>
-		<div>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		<div class="col-md-4">
+			<table class="table table-striped table-hover">
+				<tr>
+					<td>
+						ID
+					</td>
+					<td>
+						Nombre
+					</td>
+					<td>
+						Localización
+					</td>
+				</tr>
+				<tr>
+				</tr>
+			</table>
+		</div>
+		<div class="col-md-2">
+			<table class="table table-striped table-hover">
+				<tr>
+					<td>
+						ID
+					</td>
+					<td>
+						Nombre
+					</td>
+				</tr>
+				<tr>
+				</tr>
+			</table>
+		</div>
+		<div class="col-md-6">
+			<table class="table table-striped table-hover">
+				<tr>
+					<td>
+						ID
+					</td>
+					<td>
+						Nombre
+					</td>
+					<td>
+						Apellido
+					</td>
+					<td>
+						Idioma
+					</td>
+					<td>
+						Email
+					</td>
+				</tr>
+				<tr>
+				</tr>
+			</table>
 		</div>
 	</div>
 @endsection
