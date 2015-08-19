4e3b1388184613e43a4e5a403988e0d4ba9d1347
Dannypein <danieldolores117@gmail.com>
2015-08-19 15:46:45 -0500

    crear los canvas para las graficas
    se crearon los canvas para las graficas

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -7,21 +7,30 @@
 		<strong>Bienvenido:  {{ Auth::user()->nombreCompleto() }}</strong>
 	</div>
 	<div class="col-md-6">
-		<strong>Monto acumulado de la venta de postales: {{$total_Ingresadas[0]->total_SolicitudesIngresadas}}</strong>
+		<strong>Monto acumulado de la venta de postales:</strong>
 	</div>
 	<br>
 	<br>
 	<div>
-	  <div class="col-md-4">
+	  <div class="col-md-4" id="ubicaciones">
 	  	<p>Ubiciones turísticas más visitadas</p>
+		<div id="compras">
+			<canvas id="ubicacionesGrafico" width="100" height="100">
+		</div>
 	  </div>
 
 	  <div class="col-md-4">
 	  	<p>Postales más vendidas</p>
+	  	<div id="postales">
+			<canvas id="postalesGrafico" width="100" height="100">
+		</div>
 	  </div>
 
 	  <div>
 	  	<p>Idiomas más solicitados</p>
+	  	<div id="idiomas">
+			<canvas id="postalesGrafico" width="100" height="100">
+		</div>
 	  </div>
 
 	</div>
@@ -85,4 +94,35 @@
 		</div>
 	</div>
 @endsection
- 
+@section('scripts')
+			<script>
+			window.onload = cargarGraficaUbicaciones;
+
+			var chart;
+			Chart.defaults.global.responsive = true;
+
+			function cargarGraficaUbicaciones() {
+				$.ajax({
+					method: 'GET',
+					url: '{{ route('ubicaciones.index') }}',
+					data: $('#filtros').serialize(),
+					success: function(ubiciones) {
+						mostrarGraficaUbicaciones(Ubiciones);
+					}
+				});
+			}
+
+			function mostrarGraficaUbicaciones(datos) {
+				if (chart) chart.destroy();
+
+				var contexto = $('#ubicacionesGrafico').get(0).getContext('2d');
+				chart = new Chart(contexto).Line(datos);
+			}
+
+			$('#filtros').submit(function(e) {
+				e.preventDefault();
+
+				cargarGraficaUbicaciones();
+			});
+			</script>
+			@endsection
